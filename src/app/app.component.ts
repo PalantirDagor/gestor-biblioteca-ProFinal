bf39db28ab098c399746fcd1fd9db224bb65ce13
PalantirDagor <duber.alexander@gmail.com>
2025-06-04 21:04:08 -0500

    creacion menu ppal, uso ngIf ocultar/mostrar links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  usuarioLogeado: boolean = false;
   title = 'gestor-biblioteca-profinal';
 }
